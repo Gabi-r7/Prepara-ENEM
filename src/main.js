c517cd64a094d51907b9a8dad561c2cf2a8a628c
Gabriel Henrique Lamb <120254391+Gabi-r7@users.noreply.github.com>
2025-02-13 23:57:00 +0000

    removendo arquivos do projeto anterior, deixando apenas os arquivos que servirão de base para o novo projeto

---
+++ src/main.js
@@ -0,0 +1,26 @@
+
+// Exemplo de get:
+
+// async function loginVerify() {
+//     const response = await fetch('/loginVerify', {
+//         method: 'GET',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         }
+//     });
+    
+//     const responseJson = await response.json();
+//     loginStatus = responseJson; // Armazena o resultado na variável
+//     return responseJson; // Retorna o resultado
+// }
+
+
+//Exemplo de post:
+
+// const response = await fetch('/login', {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(data),
+// });
