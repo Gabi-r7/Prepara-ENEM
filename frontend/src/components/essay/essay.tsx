727b5d044bdcea4687e65d1a3ef002bd857ab66a
Gabriel Henrique Lamb <120254391+Gabi-r7@users.noreply.github.com>
2025-05-09 12:50:02 +0000

    desenvolvendo parte da redação

--- frontend/src/components/essay/essay.tsx
+++ frontend/src/components/essay/essay.tsx
@@ -5,16 +5,25 @@ function Essay() {
     return (
         <> 
             <Tittle page="Essay"/>
-            <div className="main-container">
-                <button className="btn btn-generate">Gerar tema</button>
-                <div className="motivating-text">
-                    <h2 className="motivating-text-tittle">Textos motivadores</h2>
-                    <p className="motivating-text-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <div className="all-content">
+                <div className='main-container'>
+                    <button className="btn btn-generate">Gerar tema</button>
+                    <div className="motivating-text">
+                        <h2 className="motivating-text-tittle">Textos motivadores</h2>
+                        <p className="motivating-text-content">Texto motivador aqui</p>
+                    </div>
+                    <div className="essay-text">
+                        <h2 className="essay-text-tittle">Digite seu texto: </h2>
+                        <div className='show-essay-paper'> 
+                            <div className="essay-paper-content" >
+
+                            </div>
+                        
+                        </div>
+                    </div>
                 </div>
-                <div className="essay-text">
-                    <h2 className="essay-text-tittle">Texto dissertativo argumentativo</h2>
-                    <p className="essay-text-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    
+                <div className='ads'>
+                    rapaiz
                 </div>
             </div> 
         </>
