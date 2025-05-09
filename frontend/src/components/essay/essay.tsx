62463b2601b43f59f4badd564c110945c067049c
Gabriel Henrique Lamb <120254391+Gabi-r7@users.noreply.github.com>
2025-05-09 10:42:41 +0000

    feat: unify styles for motivating and essay text sections; add content to essay section

--- frontend/src/components/essay/essay.tsx
+++ frontend/src/components/essay/essay.tsx
@@ -11,6 +11,11 @@ function Essay() {
                     <h2 className="motivating-text-tittle">Textos motivadores</h2>
                     <p className="motivating-text-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </div>
+                <div className="essay-text">
+                    <h2 className="essay-text-tittle">Texto dissertativo argumentativo</h2>
+                    <p className="essay-text-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    
+                </div>
             </div> 
         </>
         
